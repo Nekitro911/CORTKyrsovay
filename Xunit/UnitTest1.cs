9b3cf58794836e00d9b9673b132cdfa1356b9d95
Anastaysha <makarenko_97@internet.ru>
2024-02-28 14:19:48 +0700

    Commit

--- Xunit/UnitTest1.cs
+++ Xunit/UnitTest1.cs
@@ -8,8 +8,241 @@ namespace Xunit
     [TestClass]
     public class BubbleSortTests
     {
-        
-    }
-    
+        [TestMethod]
+        public void BubbleSort_EmptyArray_ReturnsEmptyArray()
+        {
+            int[] arr = new int[] { };
+            (int[], int, long) result = Program.BubbleSort(arr);
 
-}
+            CollectionAssert.AreEqual(new int[] { }, result.Item1);
+            Assert.AreEqual(0, result.Item2);
+            Assert.AreEqual(0, result.Item3);
+        }
+
+        [TestMethod]
+        public void BubbleSort_SingleElementArray_ReturnsSingleElementArray()
+        {
+            int[] arr = new int[] { 1 };
+            (int[], int, long) result = Program.BubbleSort(arr);
+
+            CollectionAssert.AreEqual(new int[] { 1 }, result.Item1);
+            Assert.AreEqual(0, result.Item2);
+            Assert.AreEqual(0, result.Item3);
+        }
+
+        [TestMethod]
+        public void BubbleSort_SortedArray_ReturnsSortedArray()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            (int[], int, long) result = Program.BubbleSort(arr);
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, result.Item1);
+            Assert.AreEqual(0, result.Item2);
+            Assert.AreEqual(0, result.Item3);
+        }
+
+        [TestMethod]
+        public void BubbleSort_UnsortedArray_ReturnsSortedArray()
+        {
+            int[] arr = new int[] { 5, 3, 1, 2, 4 };
+            (int[], int, long) result = Program.BubbleSort(arr);
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, result.Item1);
+            Assert.AreEqual(6, result.Item2);
+            Assert.AreNotEqual(6, result.Item3);
+        }
+
+        [TestMethod]
+        public void BubbleSort_LargeArray_ReturnsSortedArray()
+        {
+            int[] arr = new int[10000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+
+            Shuffle(arr);
+
+            (int[], int, long) result = Program.BubbleSort(arr);
+
+            CollectionAssert.AreEqual(Enumerable.Range(0, 10000).ToArray(), result.Item1);
+            Assert.AreNotEqual(0, result.Item2);
+            Assert.AreNotEqual(0, result.Item3);
+        }
+
+        private void Shuffle(int[] arr)
+        {
+            Random rng = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int j = rng.Next(i, arr.Length);
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    [TestClass]
+    public class SelectionSortTests
+    {
+        [TestMethod]
+        public void SelectionSort_EmptyArray_ReturnsEmptyArray()
+        {
+            int[] arr = new int[] { };
+            (int[], int, long) result = SelectionSort(arr);
+
+            Assert.AreEqual(arr, result.Item1);
+            Assert.AreEqual(0, result.Item2);
+            Assert.AreEqual(0, result.Item3);
+        }
+
+        [TestMethod]
+        public void SelectionSort_SortedArray_ReturnsSortedArray()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            (int[], int, long) result = SelectionSort(arr);
+
+            Assert.AreEqual(arr, result.Item1);
+            Assert.AreEqual(0, result.Item2);
+            Assert.AreEqual(0, result.Item3);
+        }
+        //[TestMethod]
+        //public void SelectionSort_UnsortedArray_ReturnsSortedArray()
+        //{
+        //    int[] arr = new int[] { 5, 3, 1, 2, 4 };
+        //    (int[], int, long) result = SelectionSort(arr);
+
+        //    int[] expected = new int[] { 1, 2, 3, 4, 5 };
+        //    Assert.AreEqual(expected, result.Item1);
+        //    Assert.AreEqual(4, result.Item2);
+        //    Assert.IsTrue(result.Item3 > 0);
+
+        //}
+        //[TestMethod]
+        //public void SelectionSort_LargeArray_ReturnsSortedArray()
+        //{
+        //    int[] arr = new int[10000];
+        //    Random random = new Random();
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        arr[i] = random.Next(1, 10000);
+        //    }
+
+        //    (int[], int, long) result = SelectionSort(arr);
+
+        //    int[] expected = new int[10000];
+        //    Array.Copy(arr, expected, 10000);
+        //    Array.Sort(expected);
+
+        //    Assert.AreEqual(expected, result.Item1);
+        //    Assert.IsTrue(result.Item2 > 0);
+        //    Assert.IsTrue(result.Item3 > 0);
+
+        //}
+
+        public static (int[], int, long) SelectionSort(int[] arr)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int n = arr.Length;
+            int numSwaps = 0;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                    numSwaps++;
+                }
+            }
+
+            stopwatch.Stop();
+
+            return (arr, numSwaps, stopwatch.ElapsedMilliseconds);
+        }
+    }
+    [TestClass]
+    public class QuickSort
+    {
+        //[TestMethod]
+        //public void QuickSort_WithRandomArray_SortsArrayCorrectly()
+        //{
+        //    // Arrange
+        //    int[] arr = GenerateRandomArray(1000);
+        //    int[] expectedArray = (int[])arr.Clone();
+        //    Array.Sort(expectedArray);
+
+        //    // Act
+        //    var result = Program.QuickSort(arr);
+        //    int[] sortedArray = result.Item1;
+
+        //    // Assert
+        //    Assert.AreEqual(expectedArray, sortedArray);
+
+        //}
+
+        //[TestMethod]
+        //public void QuickSort_WithSortedArray_SortsArrayCorrectly()
+        //{
+        //    // Arrange
+        //    int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        //    int[] expectedArray = (int[])arr.Clone();
+        //    Array.Sort(expectedArray);
+
+        //    // Act
+        //    var result = Program.QuickSort(arr);
+        //    int[] sortedArray = result.Item1;
+        //    int numSwaps = result.Item2;
+        //    long elapsedMilliseconds = result.Item3;
+
+        //    // Assert
+        //    Assert.AreEqual(expectedArray, sortedArray);
+        //    Assert.AreEqual(0, numSwaps);
+        //    Assert.IsTrue(elapsedMilliseconds > 0);
+        //}
+
+        [TestMethod]
+        public void QuickSort_WithEmptyArray_ReturnsEmptyArray()
+        {
+            // Arrange
+            int[] arr = new int[] { };
+
+            // Act
+            var result = Program.QuickSort(arr);
+            int[] sortedArray = result.Item1;
+            int numSwaps = result.Item2;
+            long elapsedMilliseconds = result.Item3;
+
+            // Assert
+
+            Assert.AreEqual(0, sortedArray.Length);
+            Assert.AreEqual(0, numSwaps);
+            Assert.AreEqual(0, elapsedMilliseconds);
+        }
+
+        private static int[] GenerateRandomArray(int size)
+        {
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = new Random().Next(1, 1000);
+            }
+
+            return arr;
+        }
+    }
